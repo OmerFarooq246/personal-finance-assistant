70a04c6d7267fff98bbd10385eb88b22000d1ed3
OmerFarooq246 <ofarooq.bscs21seecs@seecs.edu.pk>
2026-06-05 19:04:39 +0500

    cors header in be

--- django-backend/config/settings.py
+++ django-backend/config/settings.py
@@ -12,8 +12,8 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from datetime import timedelta
 from pathlib import Path
-from urllib.parse import urlparse
 from config.config import config
+from corsheaders.defaults import default_headers
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -148,6 +148,9 @@ MEDIA_ROOT = BASE_DIR / "media"
 
 CORS_ALLOWED_ORIGINS = config.ALLOWED_HOSTS
 CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_HEADERS = list(default_headers) + [
+    "ngrok-skip-browser-warning",
+]
 
 AUTH_USER_MODEL = "users.User"
 
